b714064518627cf8397f1378f3b6aeff230f6c9c
Luv Ratan <luv.ratan@decipherfinancials.com>
2025-09-16 20:57:20 +0530

    price vs location completed

--- Project 2 - Housing in Buenos Aires/data_wrangling_pipeline.py
+++ Project 2 - Housing in Buenos Aires/data_wrangling_pipeline.py
@@ -2,8 +2,9 @@ import pandas as pd
 
 
 def wrangle(filepath):
+    """Helps to preprocess operations for dataframe"""
     # Read CSV file into DataFrame
-    df = pd.read_csv(filepath)
+    df = pd.read_csv(filepath, index_col=0)
     
     #Subset to properties in "Capital Federal"
     df = df[df["place_with_parent_names"].str.contains("Capital Federal")]
@@ -15,9 +16,10 @@ def wrangle(filepath):
     df = df[df["price_aprox_usd"]<400_000] 
     
     #Remove outliers by "surface_covered_in_m2"
-    low,high=df["surface_covered_in_m2"].quantile([0.1,0.9])
+    low,high=df["surface_covered_in_m2"].quantile([0.05,0.95])
     df=df[df["surface_covered_in_m2"].between(low,high)]
-
+    df[["lat","lon"]]=df["lat-lon"].str.split(",", expand=True).astype(float)
+    df.drop(columns=["lat-lon"], inplace=True)
     return df
 
 
